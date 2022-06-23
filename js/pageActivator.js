42ac4eac134c7f350e8c305cfae4c8078f4f2b75
Dux <sundukows@gmail.com>
2022-06-23 21:09:26 +0200

    Правки по disable

--- js/pageActivator.js
+++ js/pageActivator.js
@@ -1,36 +1,25 @@
 const adForm = document.querySelector('.ad-form');
-const adFormHeader = document.querySelector('.ad-form-header');
-const adFormElements = document.querySelectorAll('.ad-form__element');
 const mapFilter = document.querySelector('.map__filters');
-const mapFilters = document.querySelectorAll('.map__filter');
-const mapFeature = document.querySelector('.map__features');
+const elementsForDisabling = document.querySelectorAll('fieldset , select.map__filter');
+
+const elementDisable = () => {
+  elementsForDisabling.forEach((elementForDisabling) => {
+    elementForDisabling.disabled = !elementForDisabling.disabled;
+  });
+};
 
 const pageDisable = () => {
   adForm.classList.add('ad-form--disabled');
   mapFilter.classList.add('.map__filters--disabled');
-  adFormHeader.setAttribute('disabled', 'disabled');
-  adFormElements.forEach((adFormElement)=>{
-    adFormElement.setAttribute('disabled', 'disabled');
-  });
-  mapFilters.forEach((mapFiltersElement)=>{
-    mapFiltersElement.setAttribute('disabled', 'disabled');
-  });
-  mapFeature.setAttribute('disabled', 'disabled');
+  elementDisable();
 };
-
-pageDisable();
 
 const pageActivate = () => {
   adForm.classList.remove('ad-form--disabled');
   mapFilter.classList.remove('.map__filters--disabled');
-  adFormHeader.removeAttribute('disabled');
-  adFormElements.forEach((adFormElement)=>{
-    adFormElement.removeAttribute('disabled');
-  });
-  mapFilters.forEach((mapFiltersElement)=>{
-    mapFiltersElement.removeAttribute('disabled');
-  });
-  mapFeature.removeAttribute('disabled');
+  elementDisable();
 };
+
+pageDisable();
 
 pageActivate();
