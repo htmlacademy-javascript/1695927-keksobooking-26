ecbeadb112382bc331890bc6dbb3861631fed9e6
Keks <keks@htmlacademy.ru>
2022-07-12 21:44:28 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/userForm.js
@@ -0,0 +1,157 @@
+import {doRequest} from './api.js';
+import {showSuccessMessage, showErrorMessage } from './util.js';
+import {resetAddress} from './map.js';
+
+const adForm =  document.querySelector('.ad-form');
+const MAX_PRICE = 100000;
+const MIN_LENGTH = 30;
+const MAX_LENGTH = 100;
+const MIN_VALUE_BUNGALOW = 0;
+const MIN_VALUE_FLAT = 1000;
+const MIN_VALUE_HOTEL = 3000;
+const MIN_VALUE_HOUSE = 5000;
+const MIN_VALUE_PALACE = 10000;
+const priceField = adForm.querySelector('#price');
+const timeInSelect = adForm.querySelector('#timein');
+const timeOutSelect = adForm.querySelector('#timeout');
+const roomsField = adForm.querySelector('#room_number');
+const guestsField = adForm.querySelector('#capacity');
+const typeField = adForm.querySelector('#type');
+const addressField = adForm.querySelector('#address');
+const resetButton = adForm.querySelector('.ad-form__reset');
+
+const setAddressFieldValue = (value) => {
+  addressField.value = value;
+};
+
+const RoomsValue = {
+  ONE : '1',
+  TWO: '2',
+  THREE: '3',
+  HUNDRED : '100'
+};
+
+const CapacityValue = {
+  ONE : '1',
+  TWO: '2',
+  THREE: '3',
+  UNAVAILABLE: '0'
+};
+
+const ROOM_GUEST_CAPACITY = {
+  [RoomsValue.ONE] : CapacityValue.ONE,
+  [RoomsValue.TWO] : [CapacityValue.ONE, CapacityValue.TWO],
+  [RoomsValue.THREE] : [CapacityValue.ONE, CapacityValue.TWO, CapacityValue.THREE],
+  [RoomsValue.HUNDRED] : CapacityValue.UNAVAILABLE,
+};
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'div',
+}, true);
+
+const changePriceFromType = (type) => {
+  switch (type) {
+    case 'bungalow':
+      priceField.min = MIN_VALUE_BUNGALOW;
+      priceField.placeholder = MIN_VALUE_BUNGALOW;
+      break;
+    case 'flat':
+      priceField.min = MIN_VALUE_FLAT;
+      priceField.placeholder = MIN_VALUE_FLAT;
+      break;
+    case 'hotel':
+      priceField.min = MIN_VALUE_HOTEL;
+      priceField.placeholder = MIN_VALUE_HOTEL;
+      break;
+    case 'house':
+      priceField.min = MIN_VALUE_HOUSE;
+      priceField.placeholder = MIN_VALUE_HOUSE;
+      break;
+    case 'palace':
+      priceField.min = MIN_VALUE_PALACE;
+      priceField.placeholder = MIN_VALUE_PALACE;
+      break;
+  }
+};
+
+changePriceFromType(typeField.value);
+
+const validateTitle = (value) => (
+  value.length > MIN_LENGTH-1 && value.length < MAX_LENGTH+1
+);
+
+pristine.addValidator(
+  adForm.querySelector('#title'),
+  validateTitle,
+  'Длина строки должна быть от 30 до 100 символов'
+);
+
+const validatePrice = (value) => (
+  value < MAX_PRICE+1 && value > Number(priceField.min)-1
+);
+
+pristine.addValidator(
+  priceField,
+  validatePrice,
+  'Неподходящее значение цены'
+);
+
+const validateRooms = () => (
+  ROOM_GUEST_CAPACITY[roomsField.value].includes(guestsField.value)
+);
+
+const getRoomsErrorMessage = () =>  (
+  Number(roomsField.value) === Number(RoomsValue.HUNDRED) ? 'Комнаты не для гостей' : 'Недостаточно места для размещения всех гостей'
+);
+
+pristine.addValidator(guestsField, validateRooms, getRoomsErrorMessage);
+
+typeField.addEventListener('change', () =>{
+  changePriceFromType(typeField.value);
+});
+
+
+timeInSelect.addEventListener('change', () => {
+  timeOutSelect.value = timeInSelect.value;
+});
+
+timeOutSelect.addEventListener('change', () => {
+  timeInSelect.value = timeOutSelect.value;
+});
+
+
+const onSubmitAdForm = (onSuccess, onFail) => {
+  adForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    if (pristine.validate()) {
+      doRequest(
+        () => onSuccess(),
+        () => onFail(),
+        'POST',
+        new FormData(evt.target),
+      );
+    }
+  });
+};
+
+const resetForm = () => {
+  adForm.reset();
+  changePriceFromType(typeField.value);
+  resetAddress();
+};
+
+resetForm();
+
+resetButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  resetForm();
+});
+
+onSubmitAdForm(() => {
+  showSuccessMessage();
+  resetForm();
+}, ()=> showErrorMessage());
+
+export {setAddressFieldValue, priceField};
---
+++ 11/js/util.js
@@ -0,0 +1,123 @@
+const getRandomInteger = (from, until) => {
+  if (from < 0 || until < 0) {
+    return -1;
+  }
+  if (from > until) {
+    [from, until] = [until, from];
+  }
+  return Math.floor(Math.random() * (until - from + 1)) + from;
+};
+
+const getRandom = (from, until, digits = 5) => {
+  if (from < 0 || until < 0) {
+    return -1;
+  }
+  if (from > until) {
+    [from, until] = [until, from];
+  }
+  return parseFloat((Math.random() * (until - from) + from).toFixed(digits));
+};
+
+const getRandomArrayElement = (elements) => (
+  elements[getRandomInteger(0, elements.length - 1)]
+);
+
+const getRandomArrayOfArray = (originalArray) => {
+  const maxLength = originalArray.length;
+  const lengthOfArray = getRandomInteger(1, maxLength);
+  const newArray = [];
+  while (newArray.length < lengthOfArray) {
+    const indexOfElement =  getRandomInteger(0, maxLength - 1);
+    const element = originalArray[indexOfElement];
+    if (!newArray.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
+};
+
+const switchingOfTypesHousing = (type) => {
+  switch (type) {
+    case 'hotel':
+      return 'Отель';
+    case 'palace':
+      return 'Дворец';
+    case 'house':
+      return 'Дом';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'flat':
+      return 'Квартира';
+  }
+};
+
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  switch (true) {
+    case (num % 100 > 10 && num % 100 < 21) || (num % 10 > 4 && num % 10 < 10) || (num % 10 === 0):
+      return genitivePlural;
+    case num % 10 === 1:
+      return nominative;
+    case num % 10 > 1 && num % 10 < 5:
+      return genitiveSingular;
+  }
+};
+
+const getAddress = ({ lat, lng }) => (
+  `${lat.toFixed(5)}, ${lng.toFixed(5)}`
+);
+
+const isEscEvent = (evt) => (
+  evt.key === 'Escape' || evt.key === 'Esc'
+);
+
+const closePopup = () => {
+  if (document.querySelector('.success')) {
+    document.querySelector('.success').remove();
+  }
+  if (document.querySelector('.error')) {
+    document.querySelector('.error').remove();
+  }
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
+function onPopupEscKeydown(evt) {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    closePopup();
+  }
+}
+
+const onPopupClick = () => {
+  closePopup();
+};
+
+const showSuccessMessage = () => {
+  const successTemplate = document.querySelector('#success').content.querySelector('.success');
+  const successMessage = successTemplate.cloneNode(true);
+  successMessage.style.zIndex = 1000;
+  document.querySelector('main').append(successMessage);
+  document.addEventListener('keydown', onPopupEscKeydown);
+  document.addEventListener('click', onPopupClick);
+};
+
+const showErrorMessage = (message) => {
+  const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  const errorMessage = errorTemplate.cloneNode(true);
+  errorMessage.style.zIndex = 1000;
+  if (message) {
+    errorMessage.querySelector('p').textContent = message;
+  }
+  document.querySelector('main').append(errorMessage);
+  document.addEventListener('keydown', onPopupEscKeydown);
+  document.addEventListener('click', onPopupClick);
+};
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export {getRandomInteger , getRandom , getRandomArrayElement , getRandomArrayOfArray, switchingOfTypesHousing,numDecline, getAddress,showErrorMessage, showSuccessMessage,debounce};
